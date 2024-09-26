7721c965d6df97eedf3ce1d7d122bf68f367178d
Danny <142020549+Belbel334@users.noreply.github.com>
2024-09-26 11:08:07 +0200

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,33 @@
+function xss() {
+    var user = document.getElementById("userinput").value;
+
+    const text = document.createElement("div");
+    text.innerHTML = user;
+    text.id = "box"
+
+    document.cookie = "id=pull this";
+
+    // Append to body:
+    document.body.appendChild(text);
+}
+
+function colors(){
+    var R = document.getElementById("R").value;
+    var G = document.getElementById("G").value;
+    var B = document.getElementById("B").value;
+
+    //let C = color(R,G,B);
+
+    document.getElementById("divvywivvy").innerHTML = 'current rgb value is: ' + R + ',' + G + ',' + B;
+
+    document.body.className = redBackground
+}
+
+function instagram() {
+    const img = document.createElement("img");
+    img.src = "";
+
+    document.body.appendChild(img);
+
+}
+
